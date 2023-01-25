f5e2d550498948d3e67bba50d9bb66801b59bfd4
Andrea Colleoni <andrea.colleoni@outlook.it>
2023-01-25 08:39:48 +0100

    api

---
+++ projects/jenkins-sample-api/src/main/java/info/colleoni/jenkinssampleapi/JenkinsSampleApiApplication.java
@@ -0,0 +1,13 @@
+package info.colleoni.jenkinssampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSampleApiApplication.class, args);
+	}
+
+}
---
+++ projects/jenkins-sample-api/src/test/java/info/colleoni/jenkinssampleapi/JenkinsSampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package info.colleoni.jenkinssampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
