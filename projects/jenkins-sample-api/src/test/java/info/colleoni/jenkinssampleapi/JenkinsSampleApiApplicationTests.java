6b9e87db36b9b98c8459f2bcae29b3505878b0f8
Andrea Colleoni <andrea.colleoni@outlook.it>
2023-02-02 17:43:51 +0100

    aggiunto test fallito

--- projects/jenkins-sample-api/src/test/java/info/colleoni/jenkinssampleapi/JenkinsSampleApiApplicationTests.java
+++ projects/jenkins-sample-api/src/test/java/info/colleoni/jenkinssampleapi/JenkinsSampleApiApplicationTests.java
@@ -1,5 +1,7 @@
 package info.colleoni.jenkinssampleapi;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class JenkinsSampleApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void fallire() {
+		fail();
 	}
 
 }
